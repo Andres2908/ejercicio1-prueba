22d9a765248a755caf8a7acd8d3037b0ff652aab
Andres2908 <saavedratrejo24@gmail.com>
2022-09-03 18:58:09 -0500

    listo

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import React, {Fragment ,useRef ,useState} from 'react'
+import React, {Fragment ,useEffect ,useState} from 'react'
 import {Lista} from './components/Lista'
 export function App() {
     const [array, setArray] = useState([])
-    let startArrayRef = useRef()
-    const handleStart = (value) => {
-        const number = parseInt(value) + 100
-        if(array.length > 1) setArray([])
-        for(let i = number - 100; i < number ; i++) {
+    
+    useEffect(() => {
+        handleStart(100)
+    },[])
+    const handleStart = (number) => {
+        for(let i = 1; i < number ; i++) {
             setArray(currentArray => currentArray.concat(i))
         }
     }
     return(
     <Fragment>
-        <input ref={startArrayRef} type="number" placeholder='Start' onChange={ev => handleStart(ev.target.value)}/>
         <Lista array={array}/>
     </Fragment>
     )
